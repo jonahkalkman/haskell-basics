aa1b327a7c4a544ac1a7c4cb666a7df90b6b46d3
Jonah Kalkman <jonahkalkman@MacBook-Pro-van-Jonah.local>
2021-09-12 15:21:24 +0200

    Added qsortReverse function

--- quickSort.hs
+++ quickSort.hs
@@ -1,7 +1,14 @@
-main = print (qsort [3,5,1,4,2])
+main = print (qsort [3,5,1,4,2], qsortReverse [3,5,1,4,2])
 
 qsort [] = []
 qsort (x:xs) = qsort smaller ++ [x] ++ qsort larger
                 where
                   smaller = [a | a <- xs, a <= x]
                   larger = [b | b <- xs, b > x]
+
+qsortReverse [] = []
+qsortReverse (x:xs) = qsortReverse larger ++ [x] ++ qsortReverse smaller
+                        where
+                          smaller = [a | a <- xs, a <= x]
+                          larger = [b | b <- xs, b > x]
+
