2538f8f3153411646bf681388f8677fe15359f1f
Jonah Kalkman <jonahkalkman@MacBook-Pro-van-Jonah.local>
2021-09-12 15:27:59 +0200

    Added filter doubles qsort function

--- quickSort.hs
+++ quickSort.hs
@@ -1,4 +1,4 @@
-main = print (qsort [3,5,1,4,2], qsortReverse [3,5,1,4,2])
+main = print (qsort [3,5,1,4,2], qsortReverse [3,5,1,4,2], qsortFilterDoubles [2,2,3,1,1])
 
 qsort [] = []
 qsort (x:xs) = qsort smaller ++ [x] ++ qsort larger
@@ -11,4 +11,10 @@ qsortReverse (x:xs) = qsortReverse larger ++ [x] ++ qsortReverse smaller
                         where
                           smaller = [a | a <- xs, a <= x]
                           larger = [b | b <- xs, b > x]
+
+qsortFilterDoubles [] = []
+qsortFilterDoubles (x:xs) = qsortFilterDoubles smaller ++ [x] ++ qsortFilterDoubles larger
+                              where
+                                smaller = [a | a <- xs, a < x]
+                                larger = [b | b <- xs, b > x]
 
